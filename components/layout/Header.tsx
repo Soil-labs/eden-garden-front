a4a4d6063a1bddb55c95a9e5fe0f6efc9827beee
heavensbee001 <heavensbee001@gmail.com>
2022-07-30 05:13:57 +0200

    Improve Layout

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,19 +4,21 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="mb-4 shadow-md shadow-slate-500/10">
+    <header className="pt-16 mb-4">
       <nav
-        className="relative bg-white max-w-7xl px-4 h-16 sm:px-5 lg:px-6 flex items-center justify-between"
+        className="fixed top-0 left-0 w-screen bg-white px-4 h-16 sm:px-5 lg:px-6 shadow-md shadow-slate-500/10"
         aria-label="Top"
       >
-        <Link href="/">
-          <a>
-            <span className="text-2xl">🌱</span>
-            <span className="text-soilGreen-70 text-xl font-bold ml-2">
-              Soil.
-            </span>
-          </a>
-        </Link>
+        <div className="h-full max-w-screen-xl mx-auto flex items-center justify-between">
+          <Link href="/">
+            <a>
+              <span className="text-2xl">🌱</span>
+              <span className="text-soilGreen-70 text-xl font-bold ml-2">
+                Soil.
+              </span>
+            </a>
+          </Link>
+        </div>
       </nav>
     </header>
   );
